9d1e2c427d1caa76666c828d80fffe3b4a0f3f2b
thesrijande <srijande@yahoo.com>
2020-03-28 21:11:16 +0530

    sde

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$('#particles-js').css("background-color", "rgb(0,0,0)");
+particlesJS("particles-js", {
+	"particles": {
+		"number": {
+			"value": 80,
+			"density": {
+			"enable": true,
+			"value_area": 800
+			}
+		},
+		"opacity": {
+			"random":true,
+			"value": 1,
+		},
+		"size": {
+			"value": 2,
+			"random": true,
+		},
+		"line_linked": {
+			"enable": true,
+			"distance": 175,
+			"color": "#ffffff",
+			"opacity": 0.4,
+		},
+		"move": {
+			"random": true,
+			"speed": 6,
+		}
+	},
+	"interactivity": {
+		"detect_on": "canvas",
+		"modes": {
+			"grab": {
+			"distance": 400,
+			"line_linked": {
+				"opacity": 0.8,
+				"random": true,
+				},
+				"opacity": {
+					"random":true,
+					"value": 1,
+				},
+			},
+		}
+	},
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
 
